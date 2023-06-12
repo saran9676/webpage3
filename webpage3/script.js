c9970d8d4fb5e46e51b6beee4f28908bacd4b23b
saran <saranbukkacherla@gmail.com>
2023-06-12 14:31:57 +0530

    webpage

---
+++ webpage3/script.js
@@ -0,0 +1,7 @@
+// JavaScript code
+var button = document.getElementById("myButton");
+var output = document.getElementById("output");
+
+button.addEventListener("click", function() {
+    output.innerHTML = "Button clicked!";
+});
